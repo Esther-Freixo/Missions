5a6d593fea7ab4613f0f3f357102f3605f6b767c
Esther Freixo <freixo.esther@gmail.com>
2024-03-10 17:57:02 -0300

    instala readline-sync

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-const { readMissionsData } = require("./utils/fsUtils");
+const { writeNewMissionData } = require("./utils/fsUtils");
 
 async function main() {
-    const missions = await readMissionsData()
+    const missions = await writeNewMissionData({
+        "name": "Esther Freixo",
+        "year": "1999",
+        "country": "Brazil",
+        "destination": "Space",
+        "id": 28
+    })
     console.log(missions)
 }
 
--- src/utils/fsUtils.js
+++ src/utils/fsUtils.js
@@ -2,9 +2,11 @@ const fs = require('fs').promises;
 const { log } = require('console');
 const path = require('path')
 
+const MISSION_DATA_PATH = '../../data/missions.json';
+
 async function readMissionsData() {
     try{
-        const data = await fs.readFile(path.resolve(__dirname, '../../data/missions.json'))
+        const data = await fs.readFile(path.resolve(__dirname, MISSION_DATA_PATH))
         const missions = JSON.parse(data)
         return missions
     } catch (error) {
@@ -12,8 +14,20 @@ async function readMissionsData() {
     }
 }
 
+async function writeNewMissionData(newMission) {
+    try {
+        const oldMissions = await readMissionsData()
+        const allMissions = JSON.stringify([...oldMissions, newMission])
+
+        await fs.writeFile(path.resolve(__dirname, MISSION_DATA_PATH), allMissions)
+    } catch (error) {
+        console.error(`Erro na escrita do arquivo: ${error}`)
+    }
+}
+
 module.exports = {
-    readMissionsData
+    readMissionsData,
+    writeNewMissionData
 }
 
 readMissionsData()
